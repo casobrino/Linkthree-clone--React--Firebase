f140663ec099d52b957860a600318ab170488616
Cesar Sobrino <casobrino_0@hotmail.com>
2023-07-19 22:19:41 -0600

    Updates

---
+++ precache-manifest.e143ba3bacb7769348e8986887fe4b6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "49627d548bf919685c3a",
+    "url": "/static/css/main.2a1ac314.chunk.css"
+  },
+  {
+    "revision": "49627d548bf919685c3a",
+    "url": "/static/js/main.49627d54.chunk.js"
+  },
+  {
+    "revision": "ec5ddaa9f6f0a593122a",
+    "url": "/static/js/1.ec5ddaa9.chunk.js"
+  },
+  {
+    "revision": "720f9d34fb93e6d9cbab",
+    "url": "/static/js/2.720f9d34.chunk.js"
+  },
+  {
+    "revision": "e01dcd01bb42adeaa836",
+    "url": "/static/js/runtime~main.e01dcd01.js"
+  },
+  {
+    "revision": "f456575a8082804ae60ada05f92da894",
+    "url": "/index.html"
+  }
+];
