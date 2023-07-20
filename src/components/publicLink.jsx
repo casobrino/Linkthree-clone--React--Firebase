a62c78b72edd74736477ddffc2d58a4fad55ed77
Cesar Sobrino <casobrino_0@hotmail.com>
2023-07-19 21:33:52 -0600

    Readme added

--- src/components/publicLink.jsx
+++ src/components/publicLink.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Styles from "./publicLink.module.css";
 export default function PublicLink({ linkUrl, linkTitle }) {
   return (
-    <a className={Styles.publicLinkContainer} href={linkUrl} target="_blank">
+    <a className={Styles.publicLinkContainer} href={linkUrl} target="_blank" rel="noopener noreferrer">
       <div>{linkTitle}</div>
     </a>
   );
